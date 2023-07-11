335c30cb62d4790d9d044fb5baeeeb021ff2abde
David Soares <soaresdavid1804@gmail.com>
2023-07-10 22:44:28 -0300

    atualização10/07

--- src/Jogo2.java
+++ src/Jogo2.java
@@ -5,7 +5,6 @@ public class Jogo2 {
     System.out.println("Diga o nome do seu personagem: ");
     Scanner resposta = new Scanner(System.in);
     personagem principal1 = new personagem(resposta.nextLine());
-    personagem Phillipy = new personagem("Phillipy");
     String cap1J = "INTRODUÇÃO "+principal1.Nome()+":"+principal1.Nome()+" é um presidiário da penitenciária de segurança máxima de veridium, "+
     "onde trabalha na parte de metalúrgica dela, ele está preso por roubo ao banco central da cidade Serenópolis "+
     "junto com alguns parceiros como Kenny, porém por um deslize de "+principal1.Nome()+" todos acabaram presos. Mas na noite do"+
@@ -22,157 +21,74 @@ public class Jogo2 {
     String cap4J = "Kenny pega a faca que estava na mão e esfaqueia "+principal1.Nome()+" até a morte.(Final 2)";
 
     String cap5J = ""+principal1.Nome()+" resolve sentar bem longe de Kenny, e com isso senta em uma mesa vazia"+
-    " e após isso outro presidiário senta junto dele, "+Phillipy.Nome()+"."+principal1.Nome()+" conversa com "+Phillipy.Nome()+""+
-    "e esse o revela que não aguenta mais essa prisão."+principal1.Nome()+" conta para "+Phillipy.Nome()+" seu plano?";
+    " e após isso outro presidiário senta junto dele. Phillipy"+principal1.Nome()+" conversa com Phillipy"+
+    "e esse o revela que não aguenta mais essa prisão."+principal1.Nome()+" conta para Phillipy seu plano?";
 
-    String cap6J = ""+Phillipy.Nome()+" gosta da ideia e propõe ambos se juntarem para saírem da prisão. Após ouvir as ideias de"+
-    ""+Phillipy.Nome()+", "+principal1.Nome()+" deve decidir se irá se aliar a ele ou não. O que fazer?";
+    String cap6J = "Phillipy gosta da ideia e propõe ambos se juntarem para saírem da prisão. Após ouvir as ideias de"+
+    "Phillipy, "+principal1.Nome()+" deve decidir se irá se aliar a ele ou não. O que fazer?";
 
-    String cap7J = ""+principal1.Nome()+" recusa o pedido de "+Phillipy.Nome()+" e fracassa na sua fuga da prisão e é detido"+
+    String cap7J = ""+principal1.Nome()+" recusa o pedido de Phillipy e fracassa na sua fuga da prisão e é detido"+
     "e pega mais 50 anos a mais.(final 4)";
 
-    String cap8J = ""+principal1.Nome()+" decidiu se aliar a "+Phillipy.Nome()+" e isso fez com que ambos conseguissem bolar"+
+    String cap8J = ""+principal1.Nome()+" decidiu se aliar a Phillipy e isso fez com que ambos conseguissem bolar"+
     "um plano melhor para saírem da prisão.";
 
     String cap9J = "Na noite do dia 29 ao tentar sair sem a ajuda de outra pessoa, "+principal1.Nome()+" é pego"+
     "pelos guardas e é separado dos outros presos.(Final 3)";
-//_________________________________________________________________________________________________________________________________________
-    String cap1P = "INTRODUÇÃO "+Phillipy.Nome()+": "+Phillipy.Nome()+" é outro detento presidiário da penitenciária"+
-    " de segurança máxima de veridium, trabalha na parte da prisão que cria placas para carros, e já"+
-    " faz um bom tempo que ele vem pensando em fugir. O Motivo dele está ali é o sequestro da filha de"+
-    " um cara rico. Após a chegada do dia 29 de outubro de 1995, na manhã desse dia os presos devem se"+
-    "dirigir ao refeitório, porém "+Phillipy.Nome()+" pensa se deve ir para o refeitório ou tentar fugir pela"+
-    "lavanderia onde ele sabe que há um duto de ventilação que dá do lado de fora da prisão.";
-
-    String cap2P = "Ao chegar no refeitório ele vê que um detento   "+principal1.Nome()+" está sendo provocado,"+
-    "e por saber que "+principal1.Nome()+" sabe muito de chaves pois ele era assaltante de bancos, decide fazer "+
-    "amizade com ele. "+principal1.Nome()+" deve decidir se ele espera "+principal1.Nome()+" tomar uma providência ou se ele deve"+
-    "atacar Kenny e defender "+principal1.Nome()+".";
-
-    String cap3P = ""+Phillipy.Nome()+" é pego pelos policiais e é jogado na solitária durante 2 meses. (Final 1)";
-
-    String cap4P = "Após vê que "+principal1.Nome()+" decidiu ignorar Kenny e se sentar em uma mesa distante, ele vai até lá."+
-    "Durante o caminho Kenny o para e pergunta o que ele estava olhando durante a discursão, "+Phillipy.Nome()+" manda ele se"+
-    "ferrar ou pede desculpas e segue em frente?";
-
-    String cap5P = "Após atacar Kenny eles começam uma briga e isso acaba chamando a atenção"+
-    "dos que acabam levando os dois para a solitária por um longo período. (Final 2)";
-
-    String cap6P = "Ele pede desculpas e segui para a mesa de "+principal1.Nome()+". Após seguir em frente, ele senta na mesa de"+
-    ""+principal1.Nome()+", "+principal1.Nome()+" acaba "+"contando o seu plano, "+Phillipy.Nome()+" deve chamar"+
-    ""+principal1.Nome()+" para fugirem juntos?";
-
-    String cap7P = "Por não decidir chamar "+principal1.Nome()+", o plano de "+Phillipy.Nome()+" se saí muito bem, e esse"+
-    "acaba se dando muito mal.(final 3)";
-
-    String cap8P = ""+Phillipy.Nome()+" chama "+principal1.Nome()+" para saírem daquela cadeia juntos";
-    
-    String cap9P = "Por não decidir chamar "+principal1.Nome()+", o plano de "+Phillipy.Nome()+" se saí muito bem, e esse acaba "+
-    "se dando muito mal.(final 3)";
 //------------------------------------------------------------------------------------------------------------------------------
-    String escolha1_cap1J = "fica";
-    String escolha2_cap1J = "foge";
-
-    String escolha1_cap2J = "vai para cima dele";
-    String escolha2_cap2J = "apenas ignora";
-
-    String escolha1_cap5J = "conta";
-    String escolha2_cap5J = "não conta";
-
-    String escolha1_cap6J = "se alia";
-    String escolha2_cap6J = "recusa";
-
-    String escolha1_cap1P = "refeitorio";
-    String escolha2_cap1P = "lavanderia";
-
-    String escolha1_cap2P = "ignora";
-    String escolha2_cap2P = "luta";
-
-    String escolha1_cap4P = "pede desculpas";
-    String escolha2_cap4P = "manda se ferrar";
-
-    String escolha1_cap6P = "chama";
-    String escolha2_cap6P = "se recusa";
-
-
+    String[] cap1Je = new String[2];
+    cap1Je[0] = "fica";
+    cap1Je[1] = "foge";
+    String[] cap2Je = new String[2];
+    cap2Je[0] = "vai para cima dele";
+    cap2Je[1] = "apenas ignora";
+    String[] cap5Je = new String[2];
+    cap5Je[0] = "conta";
+    cap5Je[1] = "não conta";
+    String[] cap6Je = new String[2];
+    cap6Je[0] = "se alia";
+    cap6Je[1] = "recusa";
+    String[] capFinal = new String[0];
+//--------------------------------------------------------------------------------------------------------------------------------
     //código do jogo
-    //cap1J
-    capitulo capitulo1J = new capitulo(cap1J, escolha1_cap1J, escolha2_cap1J,principal1, 0, 0);
-    capitulo capitulo2J = new capitulo(cap2J, escolha1_cap2J, escolha2_cap2J,principal1, 2, 0);
-    capitulo capitulo3J = new capitulo(cap3J, "", "", principal1, 0, 0);
-    capitulo capitulo4J = new capitulo(cap4J, "", "", principal1, 0, 2);
-    capitulo capitulo5J = new capitulo(cap5J, escolha1_cap5J, escolha2_cap5J, principal1, 4,0);
-    capitulo capitulo6J = new capitulo(cap6J, escolha1_cap6J, escolha2_cap6J, principal1, 4, 0);
-    capitulo capitulo7J = new capitulo(cap7J, "", "", principal1, 0, 4 );
-    capitulo capitulo8J = new capitulo(cap8J, "", "", principal1, 4, 0);
-    capitulo capitulo9J = new capitulo(cap9J, "", "", principal1, 0, 6);
+    capitulo capitulo1J = new capitulo(cap1J, cap1Je, principal1, 0, 0);
+    capitulo capitulo2J = new capitulo(cap2J, cap2Je, principal1, 2, 0);
+    capitulo capitulo3J = new capitulo(cap3J, cap2Je, principal1, 0, 0);
+    capitulo capitulo4J = new capitulo(cap4J, capFinal, principal1, 0, 2);
+    capitulo capitulo5J = new capitulo(cap5J, cap5Je, principal1, 4,0);
+    capitulo capitulo6J = new capitulo(cap6J, cap6Je, principal1, 4, 0);
+    capitulo capitulo7J = new capitulo(cap7J, capFinal, principal1, 0, 4 );
+    capitulo capitulo8J = new capitulo(cap8J, capFinal, principal1, 4, 0);
+    capitulo capitulo9J = new capitulo(cap9J, capFinal, principal1, 0, 6);
 
-    capitulo capitulo1P = new capitulo(cap1P, escolha1_cap1P, escolha2_cap1P, Phillipy, 0, 0);
-    capitulo capitulo2P = new capitulo(cap2P, escolha1_cap2P, escolha2_cap2P, Phillipy, 0, 0);
-    capitulo capitulo3P = new capitulo(cap3P, "", "", Phillipy, 0, 0);
-    capitulo capitulo4P = new capitulo(cap4P, escolha1_cap4P, escolha2_cap4P, Phillipy, 0, 0);
-    capitulo capitulo5P = new capitulo(cap5P, "", "", Phillipy, 0, 0);
-    capitulo capitulo6P = new capitulo(cap6P, escolha1_cap6P, escolha2_cap6P,Phillipy, 0, 0);
-    capitulo capitulo7P = new capitulo(cap7P, "", "", Phillipy, 0, 0);
-    capitulo capitulo8P = new capitulo(cap8P, "", "", Phillipy, 0, 0);
-    capitulo capitulo9P = new capitulo(cap9P, "", "", Phillipy, 0, 0);
     capitulo1J.mostraCap();
-    int valor = capitulo1J.escolha(escolha1_cap1J, escolha2_cap1J);
-    if(valor == 1){
+    int valor = capitulo1J.escolha(cap1Je);
+    if(valor == 0){
         capitulo3J.mostraCap();
     }
-    else if(valor == 2){
+    else if(valor == 1){
         capitulo2J.mostraCap();
-        int valor2 = capitulo5J.escolha(escolha1_cap2J, escolha2_cap2J);
-        if(valor2 == 1){
+        int valor2 = capitulo5J.escolha(cap2Je);
+        if(valor2 == 0){
             capitulo4J.mostraCap();
         }
-        else if(valor2 == 2){
+        else if(valor2 == 1){
             capitulo5J.mostraCap();
-            int valor3 = capitulo6J.escolha(escolha1_cap5J, escolha2_cap5J);
-            if(valor3 == 1){
+            int valor3 = capitulo6J.escolha(cap5Je);
+            if(valor3 == 0){
                 capitulo6J.mostraCap();
-                int valor4 = capitulo8J.escolha(escolha1_cap6J, escolha2_cap6J);
-                if(valor4 == 1){
+                int valor4 = capitulo8J.escolha(cap6Je);
+                if(valor4 == 0){
                     capitulo8J.mostraCap();
                 }
-                else if(valor4 == 2){
+                else if(valor4 == 1){
                     capitulo9J.mostraCap();
                 }
             }
-            else if(valor3 == 2){
+            else if(valor3 == 1){
                 capitulo7J.mostraCap();
             }
         }
-    }
-    capitulo1P.mostraCap();
-    int valor5 = capitulo1P.escolha(escolha1_cap1P, escolha2_cap2P);
-    if(valor5 == 1){
-        capitulo2P.mostraCap();
-        int valor6 = capitulo4P.escolha(escolha1_cap2P, escolha2_cap2P);
-        if(valor6 == 1){
-            capitulo4P.mostraCap();
-            int valor7 = capitulo6P.escolha(escolha1_cap4P, escolha2_cap4P);
-            if(valor7 == 1){
-                capitulo6P.mostraCap();
-                int valor8 = capitulo8P.escolha(escolha1_cap6P, escolha2_cap6P);
-                if(valor8 == 1){
-                    capitulo8P.mostraCap();
-                }
-                else if(valor8 == 2){
-                    capitulo9P.mostraCap();
-                }
-            }
-            else if(valor7 ==2){
-                capitulo7P.mostraCap();
-            }
-        }
-        else if(valor == 2){
-            capitulo5P.mostraCap();
-        }
-    }
-    else if(valor5 == 2){
-        capitulo3P.mostraCap();
     }
     resposta.close();
     } 
