a9b166ea02a2eec1a3238f994ea657a1fe0f66af
David Soares <soaresdavid1804@gmail.com>
2023-07-10 22:43:06 -0300

    atualização10/07

--- src/capitulo.java
+++ src/capitulo.java
@@ -6,36 +6,42 @@ public class capitulo {
     personagem personagem;
     int pctSucesso1;
     int pctSucesso2;
+    String[] escolhas;
     Scanner scan = new Scanner(System.in);
 
-    public capitulo(String texto, String escolha1, String escolha2, personagem personagem, int pctSucesso1, int pctSucesso2){
+    public capitulo(String texto, String[] escolhas, personagem personagem, int pctSucesso1, int pctSucesso2){
         this.texto = texto;
-        this.escolha1 = escolha1;
-        this.escolha2 = escolha2;
         this.personagem = personagem;
         this.pctSucesso1 = pctSucesso1;
         this.pctSucesso2 = pctSucesso2;
+        this.escolhas = escolhas;
     }
     public void mostraCap(){
         System.out.println(this.texto);
-        System.out.println(this.escolha1);
-        System.out.println(this.escolha2);
         personagem.GanhaPct(pctSucesso1);
         personagem.PerdePct(pctSucesso2);
+        if(escolhas != null){
+            System.out.println("as esolhas são: ");
+            for(String escolha: escolhas){
+                System.out.println(escolha);
+            }
+        }
     }
-    int escolha (String escolha1, String escolha2){
-       while(true){
-        String resposta = scan.nextLine();
-        if(resposta.equalsIgnoreCase(escolha1)){
-            return 1;
+    
+    int escolha(String[] escolhas) {
+        int resultado = -1;
+        boolean escolhaInvalida = true;
+        while (escolhaInvalida) {
+            String resposta = scan.nextLine();
+            for (int i = 0; i < this.escolhas.length; i++) {
+                if (resposta.equalsIgnoreCase(this.escolhas[i])) {
+                    resultado = i;
+                    escolhaInvalida = false;
+                } else {
+                    System.out.println("você não digitou uma opção valida! digite novamente.");
+                }
+            }
         }
-        else if(resposta.equalsIgnoreCase(escolha2)){
-            
-            return 2;
-        }
-        else{
-            System.out.println("você não digitou uma opção valida! digite novamente.");
-        }
-
-    }}
+        return resultado;
+    }
 }
