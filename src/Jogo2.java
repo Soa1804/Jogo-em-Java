b233c347ce0686c01569b822a7f21fb8e9b9613b
David Soares <soaresdavid1804@gmail.com>
2023-07-20 21:29:41 -0300

    modificação 20/07

--- src/Jogo2.java
+++ src/Jogo2.java
@@ -5,42 +5,47 @@ public class Jogo2 {
         Scanner scanner = new Scanner(System.in);
         personagem personagemPrincipal = new personagem(scanner.nextLine());
 
-        capitulo capitulo1J = new capitulo("INTRODUÇÃO " + personagemPrincipal.getNome() + ": " + personagemPrincipal.getNome() + " é um presidiário da penitenciária de segurança máxima de veridium, "
-                + "onde trabalha na parte de metalúrgica dela, ele está preso por roubo ao banco central da cidade Serenópolis "
-                + "junto com alguns parceiros como Kenny, porém por um deslize de " + personagemPrincipal.getNome()
-                + " todos acabaram presos. Mas na noite do" + " dia 28 de outubro de 1995, " + personagemPrincipal.getNome()
-                + " está pensando se ele deve tentar fugir de uma vez por todas ou continuar" + "ali até a sua sentença ser completa. O Que você decide fazer?", personagemPrincipal, 0, 0);
+        capitulo capitulo1J = new capitulo("INTRODUÇÃO :" + personagemPrincipal.getNome() +" é um presidiário da penitenciária "+
+        "de segurança máxima de veridium, onde trabalha na parte de metalúrgica dela, ele está preso por roubo ao banco central "+
+        "da cidade Serenópolis junto com alguns parceiros como Kenny, porém por um deslize de "+ personagemPrincipal.getNome()
+        +" todos acabaram presos. Mas na noite do" + " dia 28 de outubro de 1995, "+ personagemPrincipal.getNome()
+        + " está pensando se ele deve tentar fugir de uma vez por todas ou continuar ali até a sua sentença ser completa."+
+        " O Que você decide fazer?", personagemPrincipal, 0, 0);
 
         capitulo capitulo2J = new capitulo("Você decide sair e decide colocar seu plano em prática na manhã seguinte."
-                + "Durante a manhã do dia 29, " + personagemPrincipal.getNome() + ""
-                + "se encontra no refeitório e isso acaba lhe dando uma ideia, ele decide pegar duas colheres para usar uma para fabricar uma chave"
-                + " para sair de sua sela. Após passar na mesa onde Kenny estava, esse o atacou, o que " + personagemPrincipal.getNome() + " deve fazer?", personagemPrincipal, 2, 0);
+        + "Durante a manhã do dia 29, " +personagemPrincipal.getNome()+ "se encontra no refeitório e isso acaba lhe"+
+        " dando uma ideia, ele decide pegar duas colheres para usar uma para fabricar uma chave para sair de sua sela. "+
+        "Após passar na mesa onde Kenny estava, esse o atacou, o que "+personagemPrincipal.getNome()+ 
+        " deve fazer?", personagemPrincipal, 2, 0);
 
         capitulo capitulo3J = new capitulo("" + personagemPrincipal.getNome()
-                + " decide apenas cumprir sua sentença, porém já com seus 45 anos"
-                + " e sua sentença sendo de 50 anos, " + personagemPrincipal.getNome() + " acabou falecendo na prisão. (final 1)", personagemPrincipal, 0, 0);
+        + " decide apenas cumprir sua sentença, porém já com seus 45 anos e sua sentença sendo de 50 anos, " 
+        +personagemPrincipal.getNome()+" acabou falecendo na prisão. (final 1)", personagemPrincipal, 0, 0);
 
-        capitulo capitulo4J = new capitulo("Kenny pega a faca que estava na mão e esfaqueia " + personagemPrincipal.getNome() + " até a morte.(Final 2)", personagemPrincipal, 0, 2);
+        capitulo capitulo4J = new capitulo("Kenny pega a faca que estava na mão e esfaqueia "+personagemPrincipal.getNome()+
+        " até a morte.(Final 2)", personagemPrincipal, 0, 2);
 
-        capitulo capitulo5J = new capitulo("" + personagemPrincipal.getNome() + " resolve sentar bem longe de Kenny, e com isso senta em uma mesa vazia"
-                + " e após isso outro presidiário senta junto dele. Phillipy" + personagemPrincipal.getNome()
-                + " conversa com Phillipy" + personagemPrincipal.getNome()
-                + "e esse o revela que não aguenta mais essa prisão." + personagemPrincipal.getNome() + " conta para Phillipy seu plano?", personagemPrincipal, 4, 0);
+        capitulo capitulo5J = new capitulo("" +personagemPrincipal.getNome()+" resolve sentar bem longe de Kenny, e com isso"+
+        " senta em uma mesa vazia e após isso outro presidiário senta junto dele. Phillipy"+" conversa com "+personagemPrincipal.getNome()
+        + "e esse o revela que não aguenta mais essa prisão."+personagemPrincipal.getNome()+
+        " conta para Phillipy seu plano?", personagemPrincipal, 4, 0);
 
-        capitulo capitulo6J = new capitulo("Phillipy gosta da ideia e propõe ambos se juntarem para saírem da prisão. Após ouvir as ideias de"
-                + "Phillipy, " + personagemPrincipal.getNome() + " deve decidir se irá se aliar a ele ou não. O que fazer?", personagemPrincipal, 4, 0);
+        capitulo capitulo6J = new capitulo("Phillipy gosta da ideia e propõe ambos se juntarem para saírem da prisão."+
+        " Após ouvir as ideias de Phillipy, "+personagemPrincipal.getNome()+" deve decidir se irá se aliar a ele ou não."+
+        " O que fazer?", personagemPrincipal, 4, 0);
 
-        capitulo capitulo7J = new capitulo("" + personagemPrincipal.getNome()
-                + " recusa o pedido de Phillipy e fracassa na sua fuga da prisão e é detido"
-                + "e pega mais 50 anos a mais.(final 4)", personagemPrincipal, 0, 4);
+        capitulo capitulo7J = new capitulo(""+ personagemPrincipal.getNome()
+        +" recusa o pedido de Phillipy e fracassa na sua fuga da prisão e é detido"
+        +"e pega mais 50 anos a mais.(final 4)", personagemPrincipal, 0, 4);
 
-        capitulo capitulo8J = new capitulo("" + personagemPrincipal.getNome()
-                + " decidiu se aliar a Phillipy e isso fez com que ambos conseguissem bolar"
-                + " um plano melhor para saírem da prisão. E com isso sairam da prisão"
-                , personagemPrincipal, 4, 0);
+        capitulo capitulo8J = new capitulo(""+ personagemPrincipal.getNome()
+        +" decidiu se aliar a Phillipy e isso fez com que ambos conseguissem bolar"
+        +" um plano melhor para saírem da prisão. E com isso sairam da prisão"
+        , personagemPrincipal, 4, 0);
 
-        capitulo capitulo9J = new capitulo("Na noite do dia 29 ao tentar sair sem a ajuda de outra pessoa, " + personagemPrincipal.getNome() + " é pego"
-                + "pelos guardas e é separado dos outros presos.(Final 3)", personagemPrincipal, 0, 6);
+        capitulo capitulo9J = new capitulo("Na noite do dia 29 ao tentar sair sem a ajuda de outra pessoa,"+
+        " "+ personagemPrincipal.getNome() + " é pego pelos guardas e é separado dos outros presos.(Final 3)"
+        , personagemPrincipal, 0, 6);
 
         Escolha[] cap1Je = new Escolha[2];
         cap1Je[0] = new Escolha("fica", capitulo3J);
